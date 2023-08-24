6d2aa552c2663f5a550dc9a1b55ef589eca25853
oohasikazuyuki <cdok1230245@gn.iwasaki.ac.jp>
2023-08-24 16:55:56 +0900

    modify:最新版

--- src/main/java/com/todo/app/controller/TodoController.java
+++ src/main/java/com/todo/app/controller/TodoController.java
@@ -1,6 +1,6 @@
 package com.todo.app.controller;
-
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,39 +12,58 @@ import com.todo.app.mapper.TodoMapper;
 
 @Controller
 public class TodoController {
+	
+	@Autowired
+	TodoMapper todoMapper;
+	
+	@RequestMapping(value="/")
+	public String index(Model model) {
+//		List<Todo> list = todoMapper.selectAll();
+		List<Category> categoryList = todoMapper.viewCategory();
+		List<Todo> joinList = todoMapper.categoryTodo();
+		List<Todo> list = todoMapper.selectIncomplete();
+		List<Todo> doneList = todoMapper.selectComplete();
+//		List<JoinTodo> joinTodoList = todoMapper.categoryTodo();
+		model.addAttribute("todos",list);
+		model.addAttribute("doneTodos",doneList);
+		model.addAttribute("categoryTodos",categoryList);
+		model.addAttribute("joinTodos",joinList);
+//		model.addAttribute("joinTodo",joinTodoList);
+		
+		
+		return "index";
+	}
+	
+	
+	
+	@RequestMapping(value="/addCategory")
+	public String addCategory(Category category) {
+		todoMapper.addCategory(category);
+		return "redirect:/";
+	}
+	
+//	@RequestMapping(value="/viewByCategory")
+//	public String viewByCategory(JoinTodo joinTodo) {
+//		todoMapper.viewByCategory(joinTodo);
+//		return "redirect:/";
+//	}
+	
 
-    @Autowired
-    TodoMapper todoMapper;
-
-    @RequestMapping(value="/")
-    public String index(Model model) {
-
-//      List<Todo> list = todoMapper.selectAll();
-
-        List<Todo> list = todoMapper.selectIncomplete();
-        List<Todo> doneList = todoMapper.selectComplete();
-        model.addAttribute("todos",list);
-        model.addAttribute("doneTodos",doneList);
-
-        return "index";
-    }
-
-    @RequestMapping(value="/add")
-    public String add(Todo todo) {
-        todoMapper.add(todo);
-        return "redirect:/";
-    }
-
-    @RequestMapping(value="/update")
-    public String update(Todo todo) {
-        todoMapper.update(todo);
-        return "redirect:/";
-    }
-
-    @RequestMapping(value="/delete")
-    public String delete() {
-        todoMapper.delete();
-        return "redirect:/";
-    }
-
+	@RequestMapping(value="/add")
+	public String add(Todo todo) {
+		todoMapper.add(todo);
+		return "redirect:/";	
+	}
+	
+	@RequestMapping(value="/update")
+	public String update(Todo todo) {
+		todoMapper.update(todo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/delete")
+	public String delete() {
+		todoMapper.delete();
+		return "redirect:/";
+	}
 }
--- src/main/java/com/todo/app/mapper/TodoMapper.java
+++ src/main/java/com/todo/app/mapper/TodoMapper.java
@@ -1,20 +1,32 @@
 package com.todo.app.mapper;
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.todo.app.entity.Todo;
 
+
 @Mapper
 public interface TodoMapper {
-	
 	public List<Todo> selectAll();
+	
 	public List<Todo> selectIncomplete();
+	
 	public List<Todo> selectComplete();
-
+	
+	public List<Category> viewCategory();
+	
+	public List<Todo> categoryTodo();
+	
+	public void addCategory(Category category);
+	
+	//public void viewByCategory(JoinTodo joinTodo);
+	
 	public void add(Todo todo);
-
+	
 	public void update(Todo todo);
-
+	
 	public void delete();
+
 }
